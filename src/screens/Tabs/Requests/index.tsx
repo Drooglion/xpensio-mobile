fc420db2f492547c66a8d2ec615c304c06c2bea5
Paul Lorenz Buscano <buscano.paul@gmail.com>
2021-12-28 21:04:18 +0800

    Initial commit and Adds requests UI with search list

--- src/screens/Tabs/Requests/index.tsx
+++ src/screens/Tabs/Requests/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Tabs,
@@ -6,6 +6,7 @@ import {
   TabHeading,
   StyleProvider,
   View,
+  Text,
 } from 'native-base';
 
 import Header from 'library/components/Header';
@@ -17,6 +18,136 @@ import TabSelection from 'library/components/TabSelection';
 import getTheme from 'native-base-theme/components';
 import theme from 'native-base-theme/variables/theme';
 import styles from './styles';
+import { Image, SectionList, TextInput, TouchableOpacity } from 'react-native';
+import R from '../../../res/R';
+import { Icon } from 'native-base';
+
+const DATA = [
+  {
+    id: 1,
+    date: 'TODAY',
+    data: [
+      {
+        id: 123,
+        title: 'Facebook Ads',
+        description: 'New campaign',
+        amount: 10000,
+        status: 'PENDING',
+        comment: {
+          author: 'Dominick Danao',
+          content: 'Give details on where will it be used.',
+        },
+      },
+      {
+        id: 456,
+        title: 'Philippines Airlines',
+        description: 'Us Expansion',
+        amount: 25000,
+        status: 'DENIED',
+      },
+      {
+        id: 789,
+        title: 'Lazada.com',
+        description: 'Brand Video',
+        amount: 2500,
+        status: 'APPROVED',
+      },
+    ],
+  },
+  {
+    id: 2,
+    date: 'YESTERDAY',
+    data: [
+      {
+        id: 1234,
+        title: 'Facebook Ads',
+        description: 'New campaign',
+        amount: 10000,
+        status: 'PENDING',
+        comment: {
+          author: 'Dominick Danao',
+          content: 'Give details on where will it be used.',
+        },
+      },
+      {
+        id: 4567,
+        title: 'Philippines Airlines',
+        description: 'Us Expansion',
+        amount: 25000,
+        status: 'DENIED',
+      },
+      {
+        id: 8901,
+        title: 'Lazada.com',
+        description: 'Brand Video',
+        amount: 2500,
+        status: 'APPROVED',
+      },
+    ],
+  },
+  {
+    id: 3,
+    date: '2021-12-26',
+    data: [
+      {
+        id: 12345,
+        title: 'Facebook Ads',
+        description: 'New campaign',
+        amount: 10000,
+        status: 'PENDING',
+        comment: {
+          author: 'Dominick Danao',
+          content: 'Give details on where will it be used.',
+        },
+      },
+      {
+        id: 6789,
+        title: 'Philippines Airlines',
+        description: 'Us Expansion',
+        amount: 25000,
+        status: 'DENIED',
+      },
+      {
+        id: 12300,
+        title: 'Lazada.com',
+        description: 'Brand Video',
+        amount: 2500,
+        status: 'APPROVED',
+      },
+    ],
+  },
+  {
+    id: 4,
+    date: '2021-12-25',
+    data: [
+      {
+        id: 45600,
+        title: 'Facebook Ads',
+        description: 'New campaign',
+        amount: 10000,
+        status: 'PENDING',
+        comment: {
+          author: 'Dominick Danao',
+          content: 'Give details on where will it be used.',
+        },
+      },
+      {
+        id: 78900,
+        title: 'Philippines Airlines',
+        description: 'Us Expansion',
+        amount: 25000,
+        status: 'DENIED',
+      },
+      {
+        id: 12000,
+        title: 'Lazada.com',
+        description: 'Brand Video',
+        amount: 2500,
+        status: 'APPROVED',
+      },
+    ],
+  },
+];
 
 const Requests = ({ navigation }) => {
   const { t } = useTranslation();
@@ -29,6 +160,69 @@ const Requests = ({ navigation }) => {
   const goToTabPage = page => {
     tab.goToPage(page);
   };
+
+  const currencyFomart = number => {
+    return number.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  };
+
+  const [dataFiltered, setDataFiltered] = useState(DATA);
+
+  const searchData = text => {
+    const result = DATA.map(item => ({
+      ...item,
+      data: item.data.filter(dat =>
+        dat.title.toLowerCase().includes(text.toLowerCase()),
+      ),
+    })).filter(item => item.data.length > 0);
+
+    setDataFiltered(result);
+  };
+
+  const RenderItem = ({ item }) => (
+    <TouchableOpacity>
+      <View style={styles.itemContainer}>
+        <View style={styles.requestContentContainer}>
+          <View style={styles.iconContainer}>
+            <Icon name="partly-sunny-outline" style={styles.icon} />
+          </View>
+          <View style={styles.requestTitleContainer}>
+            <Text style={styles.txtTitle}>{item.title}</Text>
+            <Text style={styles.txtDescription}>{item.description}</Text>
+          </View>
+        </View>
+        <View style={styles.requestAmountContainer}>
+          <Text style={styles.txtAmount}>
+            {'\u20B1'}
+            {currencyFomart(item.amount)}
+          </Text>
+          <Text
+            style={[
+              styles.txtStatus,
+              {
+                color:
+                  item.status === 'PENDING'
+                    ? R.colors.pending
+                    : item.status === 'APPROVED'
+                    ? R.colors.success
+                    : item.status === 'DENIED'
+                    ? R.colors.error
+                    : '',
+              },
+            ]}>
+            {item.status}
+          </Text>
+        </View>
+      </View>
+      {item.comment ? (
+        <View style={styles.commentContainer}>
+          <Text style={styles.commentDetails}>"{item.comment.content}"</Text>
+          <Text style={styles.commentAuthor}>{item.comment.author}</Text>
+        </View>
+      ) : (
+        <Text></Text>
+      )}
+    </TouchableOpacity>
+  );
 
   return (
     <StyleProvider style={getTheme(theme)}>
@@ -46,6 +240,32 @@ const Requests = ({ navigation }) => {
             tabBarUnderlineStyle={styles.tabUnderline}>
             <Tab heading={<TabHeading />}>
               {/* <MyRequests onItemClick={onItemClick} /> */}
+              <View style={styles.listContainer}>
+                <View style={styles.searchContainer}>
+                  <TextInput
+                    style={styles.searchInput}
+                    placeholder="Search"
+                    onChangeText={text => searchData(text)}
+                  />
+                  <Icon name="search-sharp" style={styles.searchIcon} />
+                </View>
+                <TouchableOpacity>
+                  <Image
+                    source={require('../../../res/images/ic_filter.png')}
+                  />
+                </TouchableOpacity>
+              </View>
+              <SectionList
+                showsVerticalScrollIndicator={false}
+                style={styles.sectionContainer}
+                stickySectionHeadersEnabled={false}
+                sections={dataFiltered}
+                keyExtractor={(item, index) => item + index}
+                renderItem={RenderItem}
+                renderSectionHeader={({ section: { date } }) => (
+                  <Text style={styles.txtHeader}>{date}</Text>
+                )}
+              />
             </Tab>
             {!actAsAdmin ? null : (
               <Tab heading={<TabHeading />}>
--- src/screens/Tabs/Requests/styles.tsx
+++ src/screens/Tabs/Requests/styles.tsx
@@ -46,4 +46,102 @@ export default StyleSheet.create({
   tabUnderline: {
     opacity: 0,
   },
+  sectionContainer: {
+    paddingHorizontal: R.metrics.doubleMargin,
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  requestAmountContainer: {
+    alignItems: 'flex-end',
+  },
+  requestContentContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  requestTitleContainer: {
+    paddingLeft: 20,
+  },
+  iconContainer: {
+    backgroundColor: 'orange',
+    borderRadius: 100,
+    padding: 9,
+  },
+  icon: {
+    fontSize: 20,
+    color: 'white',
+  },
+  txtAmount: {
+    fontSize: 18,
+  },
+  txtStatus: {
+    ...R.theme.subhead,
+    fontSize: 9,
+    lineHeight: 16,
+    fontWeight: '500',
+  },
+  txtTitle: {
+    fontSize: 18,
+  },
+  txtDescription: {
+    ...R.theme.subhead,
+    fontSize: 11,
+    lineHeight: 16,
+  },
+  txtHeader: {
+    color: R.colors.subhead,
+    marginVertical: 15,
+    backgroundColor: 'white',
+    fontSize: 13,
+  },
+  commentDetails: {
+    fontSize: 13,
+    paddingBottom: 10,
+    color: R.colors.subhead,
+  },
+  commentAuthor: {
+    fontSize: 11,
+    fontStyle: 'italic',
+    color: R.colors.subhead,
+    paddingBottom: 5,
+  },
+  commentContainer: {
+    left: 58,
+    borderLeftWidth: 3,
+    borderColor: R.colors.grey,
+    paddingLeft: 10,
+    marginBottom: 25,
+    width: '70%',
+  },
+  listContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: R.metrics.doubleMargin,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 35,
+  },
+  searchInput: {
+    color: 'black',
+    backgroundColor: R.colors.searchBg,
+    borderRadius: 3,
+    fontSize: 16,
+    padding: 15,
+    paddingLeft: 40,
+  },
+  searchIcon: {
+    color: 'gray',
+    position: 'absolute',
+    fontSize: 20,
+    left: 10,
+    top: 15,
+  },
+  searchContainer: {
+    width: '90%',
+    justifyContent: 'center',
+    paddingBottom: 10,
+  },
 });
